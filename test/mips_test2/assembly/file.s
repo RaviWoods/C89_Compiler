45bed342ae5193fe30b44d295d23255f8f067358
Your Name <you@example.com>
2016-03-16 07:49:27 +0000

    "PARAMSWORK"

--- test/mips_test2/assembly/file.s
+++ test/mips_test2/assembly/file.s
@@ -1,19 +1,19 @@
 .text
 .align 2
 .globl  f
-.end  f
+.ent  f
 .type  f, @function
 f:
-sw  $4, $sp
+sw  $4, 0($sp)
 addiu $sp, $sp, -4
-sw  $5, $sp
+sw  $5, 0($sp)
 addiu $sp, $sp, -4
-sw  $6, $sp
+sw  $6, 0($sp)
 addiu $sp, $sp, -4
-sw  $7, $sp
+sw  $7, 0($sp)
 addiu $sp, $sp, -4
-lw $8, 8($sp)
-addiu $2, $8, $0
+lw $8, 12($sp)
+addu $2, $8, $0
 j	$31
 nop
 
