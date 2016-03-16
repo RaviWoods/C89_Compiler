b43c0ef56324b445ab64861f3c47a41753143596
Your Name <you@example.com>
2016-03-16 02:31:00 +0000

    "MakeAssemblyDone"

--- x_make_assembly.sh
+++ x_make_assembly.sh
@@ -1,5 +1,7 @@
 FILES="assembly_test/*.c"
 
 for f in $FILES
+do
 	fname=$(basename $f .c)
-	mips-linux-gnu-gcc  -S $f -o assembly_$fname.s
+	mips-linux-gnu-gcc  -S $f -o assembly_test/assembly/assembly_$fname.s
+done
