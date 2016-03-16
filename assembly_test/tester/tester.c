c765a08573e5e38d435cbc1b193d9094157f2ebe
Ravi <ravi.woods@gmail.com>
2016-03-16 21:55:04 +0000

    "CheckAssemblyScript"

---
+++ assembly_test/tester/tester.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int f(int a, int b);
+
+int main () {
+	int x = f(11,42);
+	printf("a = %i, b = %i\n", i,j);
+	printf("%i\n", x);
+	return 0;
+}
--- x_make_assembly.sh
+++ x_make_assembly.sh
@@ -3,5 +3,5 @@ FILES="assembly_test/*.c"
 for f in $FILES
 do
 	fname=$(basename $f .c)
-	mips-linux-gnu-gcc  -S $f -o assembly_test/assembly/assembly_$fname.s
+	mips-linux-gnu-gcc  -S $f -o assembly_test/assembly_$fname.s
 done
