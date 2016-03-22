80e6b7b6416dc07beac16ecff62b029bd11e5225
Ravi <ravi.woods@gmail.com>
2016-03-22 17:06:24 +0000

    "CPRINT5"

--- x_parser.sh
+++ x_parser.sh
@@ -13,10 +13,10 @@ fi
 for f in $FILES
 do
 	printf "\n--------------------$f START--------------------\n"
-	#cat $f
+	cat $f
 	printf "\n"
 	printf "START OF OUTPUT"
 	printf "\n---------------\n"
-	#cat $f  |  ./bin/c_parser
+	cat $f  |  ./bin/c_parser
 	printf "\n--------------------$f END----------------------\n"
 done
