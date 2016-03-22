674d8fd2560d01bdcd0a18b5038efbef889e7c58
Ravi <ravi.woods@gmail.com>
2016-03-22 19:22:31 +0000

    "LOGICALANDTEST"

--- x_spec.sh
+++ x_spec.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 FILES="c_spec_tests/*.c"
-
+echo "Specific? [y/n]"
+read text
+if [ "$text" == "y" ]; then
+	echo "Which?"
+	read filename
+	FILES="c_spec_tests/$filename.c"
+fi
 for f in $FILES
 do
 	printf "\n--------------------$f START--------------------\n"
