89f8c8daf57879d7edbd0dd90c47a07bf84eb11f
Ravi <ravi.woods@gmail.com>
2016-03-22 16:58:22 +0000

    "EditedTests"

--- x_parser.sh
+++ x_parser.sh
@@ -3,7 +3,13 @@
 FILES="test/c_files/*.c"
 
 make
-
+echo "Specific? [y/n]"
+read text
+if [ "$text" == "y" ]; then
+	echo "Which?"
+	read filename
+	FILES="test/c_files/$filename.c"
+fi
 for f in $FILES
 do
 	printf "\n--------------------$f START--------------------\n"
