3c19ba929967322f92ca1d23faceea98d022d463
Ravi <ravi.woods@gmail.com>
2016-03-24 18:15:20 +0000

    "ST11"

--- src/ast.hpp
+++ src/ast.hpp
@@ -95,6 +95,7 @@ namespace Helper {
 		int b = cont.currentStackOffset;
 		ss << "#WriteNEWVar" << name << "\n";
 		ss << "sw $9, 0($sp)\n";
+		ss << "addiu $sp, $sp, -4\n";
 		cont.variableMaps[cont.scopeIndex][name] = cont.currentStackOffset;
 		return ss.str();
 	}
