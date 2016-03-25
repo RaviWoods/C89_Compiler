322b3cd04760be168ee603d9128329fcbadd6009
Ravi <ravi.woods@gmail.com>
2016-03-25 07:55:08 +0000

    "PL22"

--- src/ast.hpp
+++ src/ast.hpp
@@ -736,7 +736,7 @@ public:
 				ss << "addiu $sp, $sp, -4\n";
 				cont.currentStackOffset++;
 				std::string x = ((*it)->cprint()); 
-				cont.variableMaps[cont.scopeIndex][x] = cont.currentStackOffset;
+				cont.variableMaps[0][x] = cont.currentStackOffset;
 				i++;
 			} else {
 				if((*it)!=NULL) {		
