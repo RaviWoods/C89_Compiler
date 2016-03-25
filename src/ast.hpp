f46320e14cbdd2bf369d9f0170e21893ec398f19
Ravi <ravi.woods@gmail.com>
2016-03-25 07:49:12 +0000

    "PL15"

--- src/ast.hpp
+++ src/ast.hpp
@@ -728,7 +728,7 @@ public:
 	std::string codeprint(Context& cont) {
 		std::stringstream ss;
 		int i = 4;
-		cout << "IN";
+		std::cout << "IN";
 		for (std::list<Parameter*>::iterator it=plist.begin(); it!=plist.end(); ++it) {
 			if(i<=7) {
 				ss << "#WriteNEWParam\n";
